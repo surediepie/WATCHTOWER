1797fcd98ebb4208e633f9128cb3887ca03afbe4
surediepie <pirupirate1234@gmail.com>
2026-08-07 22:21:20 +0530

    Fix CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,6 +48,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+
+        "https://watchtower-ns02227eo-surediepies-projects.vercel.app",
+        "https://watchtower-steel.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
