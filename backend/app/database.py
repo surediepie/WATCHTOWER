9746751e138c06daca3f056a5a14e67a2964e517
surediepie <pirupirate1234@gmail.com>
2026-08-08 00:03:41 +0530

    Support PostgreSQL database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,18 +1,30 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./watchtower.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./watchtower.db",
+)
+
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {
+        "check_same_thread": False
+    }
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args,
 )
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 
 Base = declarative_base()
